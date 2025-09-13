405f203cf9586371a099bdb732abcc8aa26d1e4a
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-09-13 11:26:17 +0530

    fix : fix UI to resolve the middleware isuue

--- Projects/project-02/Development/client/src/api/api.js
+++ Projects/project-02/Development/client/src/api/api.js
@@ -4,17 +4,21 @@ import { baseUrl } from "../util/baseUrl";
 
 const api = axios.create({
   baseURL: baseUrl,
+  withCredentials: true,
 });
 
 // Automatically add Authorization header
 api.interceptors.request.use((config) => {
-  const savedAuth = localStorage.getItem("auth");
-  if (savedAuth) {
-    const { token } = JSON.parse(savedAuth);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
+  // const savedAuth = localStorage.getItem("auth");
+
+  // console.log("DDDD", savedAuth);
+
+  // if (savedAuth) {
+  //   const { token } = JSON.parse(savedAuth);
+  //   if (token) {
+  //     config.headers.Authorization = `Bearer ${token}`;
+  //   }
+  // }
   return config;
 });
 
--- Projects/project-02/Development/client/src/api/auth.js
+++ Projects/project-02/Development/client/src/api/auth.js
@@ -1,5 +1,5 @@
 import { api } from "./api.js";
 
-export const verifyToken = () => api.get("/auth/verify");
-export const loginUser = (data) => api.post("/auth/login", data);
-export const registerUser = (data) => api.post("/auth/register", data);
+// export const verifyToken = () => api.get("/auth/verify");
+// export const loginUser = (data) => api.post("/auth/login", data);
+// export const registerUser = (data) => api.post("/auth/register", data);
