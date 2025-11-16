4f63215c4110bf568fb816a63a226e0d042e7d3f
Danishan Farookh <danishan089@gamil.com>
2025-11-16 17:07:38 +0530

    fix: bug related to permission denied

--- Projects/product-cost-calculator/server/src/middlewares/onlyTop.middleware.js
+++ Projects/product-cost-calculator/server/src/middlewares/onlyTop.middleware.js
@@ -10,13 +10,12 @@ export const onlyAdmin = (req, res, next) => {
 };
 export const onlyAdminManager = (req, res, next) => {
   const role = req.user.role;
-  if (role !== "admin" || role !== "manager") {
-    return res
-      .status(403)
-      .json({
-        success: false,
-        message: "Access denied: admin or manager role required",
-      });
+
+  if (role !== "admin" && role !== "manager") {
+    return res.status(403).json({
+      success: false,
+      message: "Access denied: admin or manager role required",
+    });
   }
 
   next();
